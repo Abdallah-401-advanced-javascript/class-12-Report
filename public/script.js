472f59293c732681928072264c8b248f09e328b6
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-19 18:54:19 +0300

    done

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 'use strict';
-require('dotenv').config();
+// require('dotenv').config();
 let URL = 'https://www.linkedin.com/oauth/v2/authorization';
-const API_SERVER = process.env.API_SERVER;
+// const API_SERVER = process.env.API_SERVER;
 let options = {
   response_type:'code',
   client_id:'77wcte95rcnlli',
-  redirect_uri: API_SERVER,
+  redirect_uri:'https://testmylocalstuff.herokuapp.com/oauth',
   scope:'r_liteprofile',
 };
 console.log(options);
