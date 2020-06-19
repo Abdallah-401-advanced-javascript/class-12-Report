abab8e501fb983f3671c9691cd6bd92be28ae316
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-19 18:34:26 +0300

    done

---
+++ public/script.js
@@ -0,0 +1,20 @@
+'use strict';
+require('dotenv').config();
+let URL = 'https://www.linkedin.com/oauth/v2/authorization';
+const API_SERVER = process.env.API_SERVER;
+let options = {
+  response_type:'code',
+  client_id:'77wcte95rcnlli',
+  redirect_uri: API_SERVER,
+  scope:'r_liteprofile',
+};
+console.log(options);
+let QueryString = Object.keys(options).map((key) => {
+  return `${key}=` + encodeURIComponent(options[key]);
+}).join('&');
+
+let authURL = `${URL}?${QueryString}`;
+console.log('@@@@@@@@@@@@@@@@@@@@@@',{authURL});
+
+let link = document.getElementById('oauth');
+link.setAttribute('href', authURL);
